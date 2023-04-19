ee14e6fcb277528ca42c3d91852179a21be85ef5
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-19 20:16:04 +0800

    code for decrypting is working

--- decryption.py
+++ decryption.py
@@ -4,13 +4,18 @@
 # PSEUDOCODE
 #user input string
 user_input = input("Enter your text: ")
+decrpt_str = ""
 # check every character in string
 for i in range(len(user_input)):
-    print(user_input[i])
 #   if character is "*", replace with "a"
+    if user_input[i] == "*":
+        decrpt_str += "a"
+    else:
+        decrpt_str += user_input[i]
 #   if character is "&", replace with "e"
 #   if character is "#", replace with "i"
 #   if character is "+", replace with "o"
 #   if character is "!", replace with "u"
 # print encrypted string
 # print output decrypted string
+print(decrpt_str)
