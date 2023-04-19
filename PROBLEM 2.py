0c84d45d1e6b12541a91e321ee308cbaa7e8adac
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-19 19:55:43 +0800

    testing inputted text

---
+++ PROBLEM 2.py
@@ -0,0 +1,16 @@
+# Assignment 2 PROBLEM 2 - DECRYPTION
+# Phoebe Rhone L. Gangoso | BSCPE 1-4
+
+# PSEUDOCODE
+#user input string
+user_input = input("Enter your text: ")
+# check every character in string
+for i in range(len(user_input)):
+    print(user_input[i])
+#   if character is "*", replace with "a"
+#   if character is "&", replace with "e"
+#   if character is "#", replace with "i"
+#   if character is "+", replace with "o"
+#   if character is "!", replace with "u"
+# print encrypted string
+# print output decrypted string
