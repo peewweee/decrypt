0abae6f4adb39753fdc572dee161d341446be79e
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-19 20:32:57 +0800

    colors and font added--the program is complete

--- decryption.py
+++ decryption.py
@@ -1,8 +1,8 @@
 # Assignment 2 PROBLEM 2 - DECRYPTION
 # Phoebe Rhone L. Gangoso | BSCPE 1-4
 
-# PSEUDOCODE
-#user input string
+import pyfiglet
+# user input string
 user_input = input("Enter your text: ")
 decrpt_str = ""
 # check every character in string
@@ -24,8 +24,12 @@ for i in range(len(user_input)):
         decrpt_str += "u"
     else:
         decrpt_str += user_input[i]
+# Center() method for title line
+line_break = "DECRYPTION"
+title_line = line_break.center(150, "*")
+print(title_line)
 # print encrypted string
-print("Encrypted text: ", user_input)
+print("\033[5;30;45m Encrypted text: ", user_input)
 # print output decrypted string
-print("The Plain Text:")
-print(decrpt_str)
+print("\033[5;31;40m The Plain Text: ")
+print(pyfiglet.figlet_format(decrpt_str, font = "basic", width = 150))
